a19cca6a0d36630234d408a889b4c05ce9b3cdec
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-29 19:41:53 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,3 @@
 fn main() {
-  println!("Based on SQL/NoSQL, data/performance/security/scalability/cost/relationships generally will vary.");
+  println!("Based on SQL/NoSQL, data/performance/security/scalability/cost/relationships generally will vary. Excel should be used to support this.");
 }
